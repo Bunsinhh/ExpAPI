072701f1e20f04dddee0b57d69e5c361d336078a
Bunsinhh <ngovbunsinh@gmail.com>
2024-05-23 23:01:45 +0700

    1Finish

---
+++ src/service/CourseService.java
@@ -0,0 +1,33 @@
+package service;
+import exception.ExceptionNotFound;
+import model.Course;
+import repository.CourseRepository;
+
+import java.util.List;
+
+public class CourseService implements Service {
+    @Override
+    public List<Course> addNewCourses() {
+        return CourseRepository.getAllCourses();
+    }
+
+    @Override
+    public List<Course> listAllCourses() {
+        return CourseRepository.listAllCourses();
+    }
+
+    @Override
+    public List<Course> findCoursesById() throws ExceptionNotFound {
+        return CourseRepository.findCourseById();
+    }
+
+    @Override
+    public List<Course> findCoursesByTitle() {
+        return CourseRepository.findCourseByTitle();
+    }
+
+    @Override
+    public List<Course> removeCourseById() {
+        return CourseRepository.removeCourseById();
+    }
+}
